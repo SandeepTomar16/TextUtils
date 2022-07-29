bec03bfc9b3a78e1a3dfb3da1656dddf6ebf1576
Sandeep Tomar <tomarsandeep38@gmail.com>
2022-07-29 22:35:57 +0530

    Improved the usability

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -45,17 +45,17 @@ export default function TextForm(props) {
             <h2 className='d-flex my-4'>{props.heading}</h2>
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 
-            <button type="button" className="btn btn-primary m-3" onClick={handleUpClick} >Convert to UpperCase</button>
-            <button type="button" className="btn btn-primary m-3" onClick={handleLoClick} >Convert to LowerCase</button>
-            <button type="button" className="btn btn-primary m-3" onClick={handleCopy} >Copy Text</button>
-            <button type="button" className="btn btn-primary m-3" onClick={handleExtraSpaces} >Remove Extra Space</button>
-            <button type="button" className="btn btn-success m-3" onClick={handleClearClick} >Clear Text</button>
+            <button type="button" className="btn btn-primary my-3 mx-3" onClick={handleUpClick} >Convert to UpperCase</button>
+            <button type="button" className="btn btn-primary my-3 mx-3" onClick={handleLoClick} >Convert to LowerCase</button>
+            <button type="button" className="btn btn-primary my-3 mx-3" onClick={handleCopy} >Copy Text</button>
+            <button type="button" className="btn btn-primary my-3 mx-3" onClick={handleExtraSpaces} >Remove Extra Space</button>
+            <button type="button" className="btn btn-success  my-3 mx-3" onClick={handleClearClick} >Clear Text</button>
 
         <h2 className='preview my-3 d-flex'>Preview your text</h2>
-        <p className='d-flex' >{text.split(" ").length} words and {text.length} characters</p>
-        <p className='d-flex'>{.008 * text.split(" ").length} Minutes read</p>
+        <p className='d-flex' >{text.split(" ").filter((element)=>{ return element.length!==0}).length} words and {text.length} characters</p>
+        <p className='d-flex'>{.008 * text.split(" ").filter((element)=>{ return element.length!==0}).length} Minutes read</p>
         <h4 className='d-flex'>Your Text Preview</h4>
-        <p className='d-flex'>{text.length>0 ? text: 'Enter something above to preview here'}</p>
+        <p className='d-flex'>{text.length>0 ? text: 'Nothing to preview!'}</p>
         </div>
     )
 }
